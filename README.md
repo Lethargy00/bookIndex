ec316396533b2a0f022713c5d7ec80990e651bab
Lethargy00 <142389181+Lethargy00@users.noreply.github.com>
2024-08-27 09:35:16 +0200

    Added css

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # bookIndex
+
 Book indexing webservice
+
+https://placehold.co/400x600?text=No+Photo&font=roboto
--- index.js
+++ index.js
@@ -5,31 +5,42 @@ fetch("books.json")
 
     data.array.forEach((book) => {
       const bookCard = document.createElement("div");
-      bookCard.classList.add("book-card");
+      bookCard.classList.add("bookCard");
+
+      // Add an event listener to the book card
+      bookCard.addEventListener("click", () => {
+        // Navigate to the book's page using the book's ID
+        window.location.href = `book.html?id=${book.id}`;
+      });
 
       const bookImage = document.createElement("img");
       bookImage.src = book.image;
       bookCard.appendChild(bookImage);
 
+      const bookTitleYearDiv = document.createElement("div");
+      bookTitleYearDiv.classList.add("bookTitle");
       const bookTitle = document.createElement("h2");
       bookTitle.textContent = book.title;
-      bookCard.appendChild(bookTitle);
+      bookTitleYearDiv.appendChild(bookTitle);
 
+      const bookYear = document.createElement("h2");
+      bookYear.textContent = `(${book.year})`;
+      bookTitleYearDiv.appendChild(bookYear);
+      bookCard.appendChild(bookTitleYearDiv);
+
+      const bookAuthorGenreISBNDiv = document.createElement("div");
       const bookAuthor = document.createElement("p");
       bookAuthor.textContent = `Author: ${book.author}`;
-      bookCard.appendChild(bookAuthor);
+      bookAuthorGenreISBNDiv.appendChild(bookAuthor);
 
       const bookGenre = document.createElement("p");
       bookGenre.textContent = `Genre: ${book.genre}`;
-      bookCard.appendChild(bookGenre);
-
-      const bookYear = document.createElement("p");
-      bookYear.textContent = `Year: ${book.year}`;
-      bookCard.appendChild(bookYear);
+      bookAuthorGenreISBNDiv.appendChild(bookGenre);
 
       const bookISBN = document.createElement("p");
       bookISBN.textContent = `ISBN: ${book.isbn}`;
-      bookCard.appendChild(bookISBN);
+      bookAuthorGenreISBNDiv.appendChild(bookISBN);
+      bookCard.appendChild(bookAuthorGenreISBNDiv);
 
       // Append the book card to the book list
       bookList.appendChild(bookCard);
