5d58bc0bf53c9398d6bfaf4372c6ed16156e0ac7
Lethargy00 <142389181+Lethargy00@users.noreply.github.com>
2024-09-04 09:13:19 +0200

    Put default values in model

--- api/Models/Book.cs
+++ api/Models/Book.cs
@@ -8,12 +8,12 @@ namespace api.Models
     public class Book
     {
         public int Id { get; set; }
-        public string Title { get; set; }
-        public string Author { get; set; }
-        public string Genre { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Author { get; set; } = string.Empty;
+        public string Genre { get; set; } = string.Empty;
         public int ReleaseDate { get; set; }
-        public string ImageURL { get; set; }
-        public string ISBN { get; set; }
+        public string ImageURL { get; set; } = "https://placehold.co/400x600?text=No+Photo&font=roboto";
+        public string ISBN { get; set; } = string.Empty;
 
     }
 }
